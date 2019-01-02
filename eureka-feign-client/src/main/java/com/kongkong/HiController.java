69ecac2c5c0dc4d46eb59259d5717736561cc1db
bikongyouran <jasonlin16@gmail.com>
2019-01-02 18:18:37 +0800

    add feign.

---
+++ eureka-feign-client/src/main/java/com/kongkong/HiController.java
@@ -0,0 +1,17 @@
+package com.kongkong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @Autowired
+    HiService hiService;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam(defaultValue = "kongkong", required = false) String name){
+        return hiService.sayHi(name);
+    }
+}
