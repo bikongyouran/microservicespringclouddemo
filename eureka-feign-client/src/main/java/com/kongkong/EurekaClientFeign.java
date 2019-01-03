e7663d30cafbf7e7dae9eafb3dcbd7660a12d851
bikongyouran <jasonlin16@gmail.com>
2019-01-03 14:04:11 +0800

    fix bug.

--- eureka-feign-client/src/main/java/com/kongkong/EurekaClientFeign.java
+++ eureka-feign-client/src/main/java/com/kongkong/EurekaClientFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eureka-client",configuration = FeignClient.class)
+@FeignClient(value = "eureka-client",configuration = FeignConfig.class)
 public interface EurekaClientFeign {
     @GetMapping(value = "/hi")
     String sayHiFromClientEureka(@RequestParam(value = "name") String name);
